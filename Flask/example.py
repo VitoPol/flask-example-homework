cf17e32fa1f10e2e6a39d63c05d4c03042e6c82c
Viktor <viktor_polyak@mail.ru>
2022-11-01 12:34:26 +0300

    -

---
+++ Flask/example.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Skypro!'
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+@app.post('/users')
+def users():
+    return 'Users', 302
+
+@app.route('/courses/<id>')
+def courses(id):
+    return f'Course id: {id}'
